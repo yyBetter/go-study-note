0b5d15a6b52c071803c561cfae3cbd79a0921ef2
wangruohang <wangruohang@luojilab.com>
2018-09-09 10:49:23 +0800

    server 添加日志配置

--- go-restful-api/demo3/main.go
+++ go-restful-api/demo3/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/lexkong/log"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"go-study-note/go-restful-api/demo2/config"
-	"go-study-note/go-restful-api/demo2/router"
-	"log"
+	"go-study-note/go-restful-api/demo3/config"
+	"go-study-note/go-restful-api/demo3/router"
 	"net/http"
 	"time"
 )
@@ -34,7 +34,6 @@ func main() {
 		// Middlwares.
 		middlewares...,
 	)
-
 	// 设置gin运行模式 debug 会打出更多日志信息
 	gin.SetMode(viper.GetString("runmode"))
 
@@ -42,11 +41,11 @@ func main() {
 		if err := pingServer(); err != nil {
 			log.Fatal("The router has no response, or it might took too long to start up.", err)
 		}
-		log.Print("The router has been deployed successfully.")
+		log.Infof("The router has been deployed successfully.")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
-	log.Printf(http.ListenAndServe(":"+viper.GetString("addr"), g).Error())
+	log.Infof("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Infof(http.ListenAndServe(":"+viper.GetString("addr"), g).Error())
 }
 
 func pingServer() error {
@@ -58,7 +57,7 @@ func pingServer() error {
 		}
 
 		// Sleep for a second to continue the next ping.
-		log.Print("Waiting for the router, retry in 1 second.")
+		log.Infof("Waiting for the router, retry in 1 second.")
 		time.Sleep(time.Second)
 	}
 	return errors.New("Cannot connect to the router.")
